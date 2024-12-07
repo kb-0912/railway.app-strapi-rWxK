ed72e3f37ff318a7a176370e9a47b51b3a4fd7ce
kb-0912 <78142541+kb-0912@users.noreply.github.com>
2024-12-07 12:43:31 +0700

    add custom content

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,17 +6,28 @@ export default ({ env }) => ({
     },
     upload: {
         config: {
-            provider: 'cloudinary',
-            providerOptions: {
-                cloud_name: env('CLOUDINARY_NAME'),
-                api_key: env('CLOUDINARY_KEY'),
-                api_secret: env('CLOUDINARY_SECRET'),
+          provider: 'aws-s3',
+          providerOptions: {
+            baseUrl: env('CDN_URL'),
+            rootPath: env('CDN_ROOT_PATH'),
+            s3Options: {
+              credentials: {
+                accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                secretAccessKey: env('AWS_ACCESS_SECRET'),
+              },
+              region: env('AWS_REGION'),
+              params: {
+                ACL: env('AWS_ACL', 'public-read'),
+                signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
+                Bucket: env('AWS_BUCKET'),
+              },
             },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-            },
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
         },
-    },
+      },
 });
